2773ad92703b358b7468c69cb9a4e079a6545ec6
vinoku <vinoku78@gmail.com>
2024-01-22 08:17:30 +0100

    Solution

--- src/main/java/com/example/PlanetUtils.java
+++ src/main/java/com/example/PlanetUtils.java
@@ -1,23 +1,26 @@
 package com.example;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PlanetUtils {
 
-  public List<Planet> getPlanetsBeginningWithM(List<Planet> planets) {
-    return null;
-  }
+    public List<Planet> getPlanetsBeginningWithM(List<Planet> planets) {
+        return planets.stream()
+                .filter(planet -> planet.getName().startsWith("M"))
+                .toList();
+    }
 
-  public List<Planet> getPlanetsWithRings(List<Planet> planets) {
-    return null;
-  }
+    public List<Planet> getPlanetsWithRings(List<Planet> planets) {
+        return planets.stream().filter(planet -> planet.getHasRings()).toList();
+    }
 
-  public List<Planet> getPlanetsWithMoreThanThreeMoons(List<Planet> planets) {
-    return null;
-  }
+    public List<Planet> getPlanetsWithMoreThanThreeMoons(List<Planet> planets) {
+        return planets.stream().filter(planet -> planet.getNumberOfMoons() > 3).toList();
+    }
 
-  public List<Planet> getPlanetsWithDensityGreaterThanFive(List<Planet> planets) {
-    return null;
-  }
+    public List<Planet> getPlanetsWithDensityGreaterThanFive(List<Planet> planets) {
+        return planets.stream().filter(planet -> planet.getDensity() > 5).collect(Collectors.toList());
+    }
 
 }
